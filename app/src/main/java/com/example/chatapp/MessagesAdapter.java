46510ff47b699d3be6ce7518100f339d979b5a41
Denys <deniskole1@gmail.com>
2019-04-04 18:47:15 +0300

    shange images

--- app/src/main/java/com/example/chatapp/MessagesAdapter.java
+++ app/src/main/java/com/example/chatapp/MessagesAdapter.java
@@ -1,10 +1,58 @@
 package com.example.chatapp;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessagesViewHolder> {
 
 
-public class MessagesAdapter {
+    private List<Message> messages;
 
-    public class MessagesViewHolder ex
 
+    public MessagesAdapter(List<Message> messages) {
+        messages = new ArrayList<>();
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MessagesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_item_message, viewGroup, false);
+        return new MessagesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessagesViewHolder messagesViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    class MessagesViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewAuthor;
+        private TextView textViewMessage;
+
+        public MessagesViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewAuthor = itemView.findViewById(R.id.textViewAuthor);
+            textViewMessage = itemView.findViewById(R.id.editTextMessage);
+        }
+    }
 
 }
